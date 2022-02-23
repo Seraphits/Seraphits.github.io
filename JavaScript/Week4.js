6de55c8524d3ba5618362373ae7c92e2787b9c6c
Seraphits <97304529+Seraphits@users.noreply.github.com>
2022-02-23 17:24:17 -0500

    Comment Week4

--- JavaScript/Week4.js
+++ JavaScript/Week4.js
@@ -1,20 +1,17 @@
-// var fname = "Jack";
-// var age = 23;
-// var message = "Hi, my name is " + fname + " and I am " + age + " year old";
-// alert(message);
-
 function playButton() {
 //    Declaration local varables
     var anwser, myElement;
-
+    // Asked the user a question
     anwser = prompt('What animal says "meow"?').toLowerCase();
-
+    // Pulls Question
     myElement = document.getElementById("diamond");
 
     if (anwser === "cat") {
+        // when they get the Qu right
         document.body.style.background = "green";
         myElement.style.display = "flex";
     } else {
+        // when they get Qu wrong
         document.body.style.background = "red";
         myElement.style.display = "none";
     }
