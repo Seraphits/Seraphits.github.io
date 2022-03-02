a8984bb1ad96cb728d292ae10eccd7019af8a85b
Seraphits <97304529+Seraphits@users.noreply.github.com>
2022-03-01 22:02:20 -0500

    Week5

---
+++ JavaScript/Week5.js
@@ -0,0 +1,4 @@
+function info_TCMS() {
+    //    Declaration local varables
+       alert("Rockit School focuses on STEM education. Flamingo School is for artist musicians and dancers. Water School has a high emphasis on sports and outdoor activities");
+    }
