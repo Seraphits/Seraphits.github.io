25e4ba45b924c6ab3724d7ee8bdb728ad1e644cc
Seraphits <97304529+Seraphits@users.noreply.github.com>
2022-04-03 10:03:36 -0400

    Week8 working

--- JavaScript/Week8.js
+++ JavaScript/Week8.js
@@ -12,13 +12,14 @@ function create_list() {
         {
             my_array[i] = prompt("Type album name here");  
             console.log(my_array);  
+            output_array()
         }  
     }
     else
     {
         alert("You've already created your collection of albums.  Please use another function.");
     }
-}
+};
 function addWord() {
     var my_form_value
     console.log(my_form_value);
@@ -28,7 +29,9 @@ function addWord() {
     console.log(x);
     my_array.push(prompt("Push another city to the end of the array"));
     alert("After push the cities are " + my_array );
-}
+    output_array()
+};
+
 function output_array() {
     word_string = new String();
     console.log("start word:" + word_string);
@@ -50,4 +53,54 @@ function output_array() {
     // alert("My array had these elements in it: " + word_string);
     var out = document.getElementById("output");
     out.innerHTML = "My array had these elements in it: " + word_string;
-}
+};
+
+// createList.addEventListener('click', output_array());
+
+// newWord.addEventListener('click', output_array());
+
+// createList.addEventListener('click', function() {
+//     word_string = new String();
+//     console.log("start word:" + word_string);
+//     for (var i=0; i<my_array.length; i++)
+//     {
+//         if (i == my_array.length-1)
+//         {
+//             word_string += "and ";
+//             word_string += my_array[i];
+//             console.log("word list:" + word_string);
+
+//         }
+//         else
+//         {
+//         word_string += my_array[i];
+//         word_string += ", ";
+//         }
+//     }
+//     // alert("My array had these elements in it: " + word_string);
+//     var out = document.getElementById("output");
+//     out.innerHTML = "My array had these elements in it: " + word_string;
+// })
+
+// newWord.addEventListener('click', function() {
+//     word_string = new String();
+//     console.log("start word:" + word_string);
+//     for (var i=0; i<my_array.length; i++)
+//     {
+//         if (i == my_array.length-1)
+//         {
+//             word_string += "and ";
+//             word_string += my_array[i];
+//             console.log("word list:" + word_string);
+
+//         }
+//         else
+//         {
+//         word_string += my_array[i];
+//         word_string += ", ";
+//         }
+//     }
+//     // alert("My array had these elements in it: " + word_string);
+//     var out = document.getElementById("output");
+//     out.innerHTML = "My array had these elements in it: " + word_string;
+// })
