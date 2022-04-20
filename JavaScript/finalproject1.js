5ee6beb9f55737048374d0ee02334031dab2d419
Seraphits <97304529+Seraphits@users.noreply.github.com>
2022-04-20 09:42:23 -0400

    Final project

---
+++ JavaScript/finalproject1.js
@@ -0,0 +1,37 @@
+colors = {
+    color: "#CFB997",
+    colorName: "Beige",
+    lifeState: "Survival Processes",
+}, {
+    color: "#B52CC2",
+    colorName: "Purple",
+    lifeState: "Tribe-Like Groups",
+}, {
+    color: "#EB0909",
+    colorName: "Red",
+    lifeState: "Power Seeking",
+}, {
+    color: "#3138BF",
+    colorName: "Blue",
+    lifeState: "Purposeful",
+}, {
+    color: "#E46409",
+    colorName: "Orange",
+    lifeState: "Autonomy-Seeking",
+}, {
+    color: "#248755",
+    colorName: "Green",
+    lifeState: "Experiential and Consensual",
+}, {
+    color: "#F8F700",
+    colorName: "Yellow",
+    lifeState: "Integrative",
+}, {
+    color: "#40E0D0",
+    colorName: "Turquoise",
+    lifeState: "Holistic",
+}, {
+    color: "#FF7F50",
+    colorName: "Coral",
+    lifeState: "Master of chaos",
+}
