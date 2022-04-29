499ec9e0289c73d8dbcc62d63dd51c9738b1cd8c
Seraphits <97304529+Seraphits@users.noreply.github.com>
2022-04-29 17:11:06 -0400

    j

---
+++ img/Document_files/fanalproject.js
@@ -0,0 +1,61 @@
+// store var
+const draggableElements = document.querySelectorAll(".draggable");
+const droppableElements = document.querySelectorAll(".droppable");
+
+// listen for when an object is dragged
+draggableElements.forEach(elem => {
+  elem.addEventListener("dragstart", dragStart); //  starts dragging an item 
+});
+
+// listen for when an object is drogged
+droppableElements.forEach(elem => {
+  elem.addEventListener("dragenter", dragEnter); //  a dragged item enters a valid drop target
+  elem.addEventListener("dragover", dragOver); //  dragged item is being dragged over the drop target
+  elem.addEventListener("dragleave", dragLeave); // dragged item leaves the drop target
+  elem.addEventListener("drop", drop); // item is dropped on the drop target
+});
+
+// Drag and Drop Functions
+
+//Events fired on the drag target
+
+function dragStart(event) {
+  event.dataTransfer.setData("text", event.target.id); 
+}
+
+//Events fired on the drop target
+
+function dragEnter(event) {
+  if(!event.target.classList.contains("dropped")) {
+    event.target.classList.add("droppable-hover");
+  }
+}
+
+function dragOver(event) {
+  if(!event.target.classList.contains("dropped")) {
+    event.preventDefault(); // Prevent default to allow drop
+  }
+}
+
+function dragLeave(event) {
+  if(!event.target.classList.contains("dropped")) {
+    event.target.classList.remove("droppable-hover");
+  }
+}
+
+function drop(event) {
+  event.preventDefault(); //  prevent the browser default handling of the data
+  event.target.classList.remove("droppable-hover");
+  const draggableElementData = event.dataTransfer.getData("text"); // Get the dragged data. 
+  const droppableElementData = event.target.getAttribute("data-draggable-id");
+  const isCorrectMatching = draggableElementData === droppableElementData;
+  if(isCorrectMatching) {
+    const draggableElement = document.getElementById(draggableElementData);
+    event.target.classList.add("dropped");
+    // event.target.style.backgroundColor = draggableElement.style.color; // This approach works only for inline styles. A more general approach would be the following: 
+    event.target.style.backgroundColor = window.getComputedStyle(draggableElement).color;
+    draggableElement.classList.add("dragged");
+    draggableElement.setAttribute("draggable", "false");
+    // event.target.insertAdjacentHTML("afterbegin", `<i class="fas fa-${draggableElementData}"></i>`);
+  }
+}
